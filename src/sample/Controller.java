9bedea269f7c1757b2de6ab75ed8dcb8382e0c3e
radcoolman <bradcoleman1998@gmail.com>
2018-09-14 23:40:39 +1200

    Adding basic functionality for the database - populating left side list, adding a simple action listener

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,31 +8,65 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 import javafx.util.StringConverter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Controller {
-    @FXML
-    private Button _practice;
-    @FXML
-    private Button _testMic;
-    @FXML
-    private CheckBox _shuffle;
-    @FXML
-    private TitledPane _currentName;
-    @FXML
-    private TextArea _nameDetails;
-    @FXML
-    private ListView _playlist;
-    @FXML
-    private ListView _names;
+    @FXML private Button _practice;
+    @FXML private Button _testMic;
+    @FXML private CheckBox _shuffle;
+    @FXML private TextArea _nameDetails;
+    @FXML private ListView _playlist;
+    @FXML private ListView _names;
+
 
     /**
      * Initializes the controller class.
      */
     @FXML
     private void initialize() {
+        // Initializing the list of names
+        ListView<Name> listView = new ListView<>();
+        File folder = new File("names");
+        File[] listOfFiles = folder.listFiles();
+        ObservableList<Name> names = FXCollections.observableArrayList();
+        for (File file : listOfFiles) {
+            names.add(new Name(file.getName()));
+        }
+        _names.setItems(names);
 
-        
+        // Allows the ListView to show Strings of the names, while each index refers
+        // to a Name object
+        _names.setCellFactory(new Callback<ListView<Name>, ListCell<Name>>(){
+
+            @Override
+            public ListCell<Name> call(ListView<Name> p) {
+
+                ListCell<Name> cell = new ListCell<Name>(){
+
+                    @Override
+                    protected void updateItem(Name t, boolean bln) {
+                        super.updateItem(t, bln);
+                        if (t != null) {
+                            setText(t.toString());
+                        }
+                    }
+
+                };
+
+                return cell;
+            }
+        });
+
+    }
+
+    @FXML
+    private void nameSelected() {
+        _nameDetails.setText(_names.getSelectionModel().getSelectedItem().toString());
     }
 }
---
+++ src/sample/Name.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents a name in the database.
+ * Holds information about the past attempts and rating.
+ */
+public class Name {
+    private String _name;
+    private int _rating;
+
+    public Name(String fileName) {
+        Pattern p = Pattern.compile("_([a-zA-Z]*)\\.wav");
+        Matcher m = p.matcher(fileName);
+        if (m.find()) {
+            _name = m.group(1);
+        } else {
+            _name = fileName;
+        }
+
+    }
+
+    public String toString() {
+        return _name;
+    }
+}
