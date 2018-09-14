8cae801afd57e7085271266777d63122b1f16314
radcoolman <bradcoleman1998@gmail.com>
2018-09-14 17:36:57 +1200

    Making layout for main screen.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,26 +6,26 @@ import javafx.event.ActionEvent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.util.StringConverter;
 
 
 public class Controller {
     @FXML
-    private Button practice;
+    private Button _practice;
     @FXML
-    private Text currentNames;
+    private Button _testMic;
     @FXML
-    private ListView names;
+    private CheckBox _shuffle;
+    @FXML
+    private TitledPane _currentName;
+    @FXML
+    private TextArea _nameDetails;
+    @FXML
+    private ListView _playlist;
+    @FXML
+    private ListView _names;
 
     /**
      * Initializes the controller class.
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 439,236));
+        primaryStage.setScene(new Scene(root, 540,300));
         primaryStage.show();
     }
 
